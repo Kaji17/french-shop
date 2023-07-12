a64191b57f4051f885dbd8aa48eed1d6356f8c03
Katina Ouattara <ouattarapevirgile@gmail.com>
2023-07-12 10:08:14 +0000

    initial commit

---
+++ src/main/java/com/pevir/frenchshop/FrenchShopApplication.java
@@ -0,0 +1,13 @@
+package com.pevir.frenchshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenchShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenchShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pevir/frenchshop/FrenchShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pevir.frenchshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrenchShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
